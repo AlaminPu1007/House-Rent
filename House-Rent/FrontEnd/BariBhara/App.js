398603994952eca0eddc4f73f9a8d58b8cfca15b
AlaminPu1007 <alamin66.sit@gmail.com>
2022-04-23 20:06:41 +0600

    react-navigation version 6 npm module is added

--- House-Rent/FrontEnd/BariBhara/App.js
+++ House-Rent/FrontEnd/BariBhara/App.js
@@ -1,14 +1,34 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const App = () => {
+function HomeScreen() {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
+}
+function ScreenScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Setting Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={ScreenScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
-
-const styles = StyleSheet.create({});
