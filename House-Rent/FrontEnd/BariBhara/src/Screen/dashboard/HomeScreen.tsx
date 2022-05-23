7052d94d7a954f550ee79aae0d23166c37308b5f
AlaminPu1007 <alamin66.sit@gmail.com>
2022-05-23 20:47:36 +0600

    BottomTabScreenProps added

--- House-Rent/FrontEnd/BariBhara/src/Screen/dashboard/HomeScreen.tsx
+++ House-Rent/FrontEnd/BariBhara/src/Screen/dashboard/HomeScreen.tsx
@@ -1,7 +1,12 @@
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import {RootStackParamList} from '../../NavigationFlow';
 
-const HomeScreen = () => {
+//get screen props
+type Props = BottomTabScreenProps<RootStackParamList, 'Setting'>;
+
+const HomeScreen = ({navigation}: Props) => {
   return (
     <View>
       <Text>HomeScreen</Text>
