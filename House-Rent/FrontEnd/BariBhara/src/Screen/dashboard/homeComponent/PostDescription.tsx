98045012a8dcde856c738b76c8e427b2ebcd6f92
AlaminPu1007 <alamin66.sit@gmail.com>
2022-06-19 20:02:00 +0600

    Text multiple line to more button has been created

--- House-Rent/FrontEnd/BariBhara/src/Screen/dashboard/homeComponent/PostDescription.tsx
+++ House-Rent/FrontEnd/BariBhara/src/Screen/dashboard/homeComponent/PostDescription.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import ConstValue from '../../../component/ConstValue';
 import Color from '../../../component/Color';
 import {heightToDp} from '../../../component/Responsive';
@@ -7,13 +7,36 @@ import {heightToDp} from '../../../component/Responsive';
 const ColorValue = Color();
 
 const PostDescription = ({data}: any) => {
+  // define use state here
+  const [activeMoreButton, setActiveMoreButton] = useState<number>(3);
+
+  // get text total number of lines
+  const onTextLayout = (e: any) => {
+    const Lines = e.nativeEvent.lines.length;
+    if (Lines > 3) {
+      setActiveMoreButton(e.nativeEvent.lines.length);
+    } else {
+      setActiveMoreButton(3);
+    }
+  };
   return (
     <View style={styles.container}>
       <TouchableOpacity activeOpacity={0.8} style={styles.descriptionButton}>
-        <Text numberOfLines={3} style={styles.descriptionText}>
+        <Text
+          onTextLayout={onTextLayout}
+          numberOfLines={3}
+          style={styles.descriptionText}>
           {data.description}
         </Text>
       </TouchableOpacity>
+      {/* add more button */}
+      <View>
+        {activeMoreButton > 3 ? (
+          <TouchableOpacity>
+            <Text>More</Text>
+          </TouchableOpacity>
+        ) : null}
+      </View>
     </View>
   );
 };
