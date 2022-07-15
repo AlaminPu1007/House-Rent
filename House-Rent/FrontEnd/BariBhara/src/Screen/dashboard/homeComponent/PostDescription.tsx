6f8fa926ffafafeb89c0be033023979639d20b75
AlaminPu1007 <alamin66.sit@gmail.com>
2022-07-15 12:24:13 +0600

    Post description more and less method has been added

--- House-Rent/FrontEnd/BariBhara/src/Screen/dashboard/homeComponent/PostDescription.tsx
+++ House-Rent/FrontEnd/BariBhara/src/Screen/dashboard/homeComponent/PostDescription.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import ConstValue from '../../../component/ConstValue';
 import Color from '../../../component/Color';
 import {heightToDp} from '../../../component/Responsive';
@@ -8,32 +8,35 @@ const ColorValue = Color();
 
 const PostDescription = ({data}: any) => {
   // define use state here
-  const [activeMoreButton, setActiveMoreButton] = useState<number>(3);
+  const [activeMoreButton, setActiveMoreButton] = useState<boolean>(false);
+  const [textShown, setTextShown] = useState<boolean>(false);
 
   // get text total number of lines
-  const onTextLayout = (e: any) => {
-    const Lines = e.nativeEvent.lines.length;
-    if (Lines > 3) {
-      setActiveMoreButton(e.nativeEvent.lines.length);
-    } else {
-      setActiveMoreButton(3);
-    }
+
+  const onTextLayout = useCallback((e: any) => {
+    setActiveMoreButton(e.nativeEvent.lines.length > 3); //to check the text is more than 3 lines or not
+  }, []);
+
+  // Expand description
+  const toggleNumberOfLines = () => {
+    //To toggle the show text or hide it
+    setTextShown(!textShown);
   };
   return (
     <View style={styles.container}>
       <TouchableOpacity activeOpacity={0.8} style={styles.descriptionButton}>
         <Text
           onTextLayout={onTextLayout}
-          numberOfLines={3}
+          numberOfLines={textShown ? undefined : 3}
           style={styles.descriptionText}>
           {data.description}
         </Text>
       </TouchableOpacity>
       {/* add more button */}
       <View style={styles.moreView}>
-        {activeMoreButton > 3 ? (
-          <TouchableOpacity>
-            <Text style={styles.moreText}>More</Text>
+        {activeMoreButton ? (
+          <TouchableOpacity activeOpacity={0.5} onPress={toggleNumberOfLines}>
+            <Text style={styles.moreText}> {textShown ? 'Less' : 'More'}</Text>
           </TouchableOpacity>
         ) : null}
       </View>
