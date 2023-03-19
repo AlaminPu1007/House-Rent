0e9ed0baf229f454420c109af17c403ad0bdb20b
alaminpu1007 <alamin66.sit@gmail.com>
2023-03-19 22:40:12 +0600

    resolved some error issue, that project is not running with new env setting

--- House-Rent/FrontEnd/BariBhara/src/NavigationFlow.tsx
+++ House-Rent/FrontEnd/BariBhara/src/NavigationFlow.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // bring auth screen
